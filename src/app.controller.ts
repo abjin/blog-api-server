6814c807fdb5ac3ed2352869a614acc2e391528d
abjin <abjin69@gmail.com>
2024-05-27 18:06:27 +0900

    delete unused file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { randomInt } from 'crypto';
+
+const startedAt = new Date();
+const randomNumber = randomInt(0, 100);
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return `${startedAt.toISOString()}, ${randomNumber}`;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
+  imports: [CategoryModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
