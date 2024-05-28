1f9972b67993afb5b2b026d257b886ab95132d75
abjin <abjin69@gmail.com>
2024-05-28 15:36:42 +0900

    add post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { CategoriesModule } from './categories/categories.module';
 import { DbModule } from './db/db.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [CategoriesModule, DbModule],
+  imports: [CategoriesModule, DbModule, PostsModule],
   controllers: [AppController],
 })
 export class AppModule {}
