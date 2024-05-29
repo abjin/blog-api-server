f837a1a7717b6d4fbabc22211a51a124be8f7e43
abjin <abjin69@gmail.com>
2024-05-29 15:16:10 +0900

    add class validator

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  app.enableShutdownHooks();
+
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableShutdownHooks();
+
   await app.listen(port);
+
   console.log({ port, data: new Date() });
 }
 bootstrap();
