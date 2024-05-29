37e3daaae9326de59901ffdcbdcb286a08118ec4
abjin <abjin69@gmail.com>
2024-05-29 15:36:25 +0900

    get posts by category id

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { GetPostsRequestQueryDto } from './get-posts-request-query.dto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPosts(@Query() query: GetPostsRequestQueryDto) {
+    return this.postsService.getPostsByCategory(query.categoryId);
+  }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/db/prisma.service';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  getPostsByCategory(categoryId: string) {
+    return this.prismaService.post.findMany({ where: { categoryId } });
+  }
+}
