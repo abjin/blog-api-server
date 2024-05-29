128d990102e98d7ded5345b7b9659d83677e7e03
abjin <abjin69@gmail.com>
2024-05-29 15:08:28 +0900

    fix port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 3000;
   app.enableShutdownHooks();
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(port);
