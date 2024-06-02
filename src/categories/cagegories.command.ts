05f3850c05d83471b2b79b8cb7efc5a01ee71156
abjin <abjin69@gmail.com>
2024-06-02 17:02:14 +0900

    create seed data

--- src/categories/cagegories.command.ts
+++ src/categories/cagegories.command.ts
@@ -6,8 +6,25 @@ import { PrismaService } from 'src/db/prisma.service';
 export class CategoriesCommand {
   constructor(private readonly prismaService: PrismaService) {}
 
+  @Command({ command: 'post:categories' })
+  async postCategories() {
+    return this.prismaService.category.createMany({
+      data: [
+        { id: 'INTRO', title: '회사소개' },
+        {
+          id: 'EXAMPLE',
+          title: '구축사례',
+        },
+        {
+          id: 'NOTICE',
+          title: '공지사항',
+        },
+      ],
+    });
+  }
+
   @Command({ command: 'get:all:categories' })
-  async getAllPosts() {
+  async getAllCategories() {
     const categories = await this.prismaService.category.findMany();
     console.log(categories);
   }
--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { CategoriesCommand } from './cagegories.command';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService],
+  providers: [CategoriesService, CategoriesCommand],
 })
 export class CategoriesModule {}
