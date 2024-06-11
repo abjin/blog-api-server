886c5cbbce3a6e0e2d8c2c24ed40b87d749227cf
abjin <abjin69@gmail.com>
2024-06-11 16:45:09 +0900

    delete using command

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
-import { CategoriesCommand } from './cagegories.command';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService, CategoriesCommand],
+  providers: [CategoriesService],
 })
 export class CategoriesModule {}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-import { PostsCommand } from './posts.command';
 
 @Module({
   controllers: [PostsController],
-  providers: [PostsService, PostsCommand],
+  providers: [PostsService],
 })
 export class PostsModule {}
