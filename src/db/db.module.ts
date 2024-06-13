5e183877972f60fa6c2ed95dfa22268f9cb6831b
abjin <abjin69@gmail.com>
2024-06-13 21:57:15 +0900

    test

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -2,9 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { APP_FILTER } from '@nestjs/core';
 import { PrismaClientExceptionFilter } from 'src/db/prisma-exception.filter';
+import { EmailModule } from '@libs/email';
 
 @Global()
 @Module({
+  imports: [EmailModule],
   providers: [
     PrismaService,
     { provide: APP_FILTER, useClass: PrismaClientExceptionFilter },
--- src/db/prisma.service.ts
+++ src/db/prisma.service.ts
@@ -1,16 +1,36 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { EmailService } from '@libs/email';
+import {
+  Inject,
+  Injectable,
+  OnApplicationShutdown,
+  OnModuleInit,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnApplicationShutdown
+{
   constructor(private readonly configService: ConfigService) {
     super({
       datasources: { db: { url: configService.getOrThrow('DATABASE_URL') } },
     });
   }
 
+  @Inject() private readonly emailService: EmailService;
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onApplicationShutdown(signal: string) {
+    await this.emailService.sendEmail({
+      to: this.configService.get('GMAIL_USERNAME'),
+      subject: 'onApplicationShutdown 테스트',
+      html: 'onApplicationShutdown 테스트 <br/>TODO backup sqlite',
+    });
+    console.log(signal); // e.g. "SIGINT"
   }
 }
