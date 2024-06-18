2c44f1a2ebaa05004df9c09f411844d7de37855a
abjin <abjin69@gmail.com>
2024-06-18 21:41:07 +0900

    Revert "test"
    This reverts commit 5e183877972f60fa6c2ed95dfa22268f9cb6831b.

--- src/db/prisma.service.ts
+++ src/db/prisma.service.ts
@@ -1,36 +1,16 @@
-import { EmailService } from '@libs/email';
-import {
-  Inject,
-  Injectable,
-  OnApplicationShutdown,
-  OnModuleInit,
-} from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnApplicationShutdown
-{
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor(private readonly configService: ConfigService) {
     super({
       datasources: { db: { url: configService.getOrThrow('DATABASE_URL') } },
     });
   }
 
-  @Inject() private readonly emailService: EmailService;
-
   async onModuleInit() {
     await this.$connect();
-  }
-
-  async onApplicationShutdown(signal: string) {
-    await this.emailService.sendEmail({
-      to: this.configService.get('GMAIL_USERNAME'),
-      subject: 'onApplicationShutdown 테스트',
-      html: 'onApplicationShutdown 테스트 <br/>TODO backup sqlite',
-    });
-    console.log(signal); // e.g. "SIGINT"
   }
 }
